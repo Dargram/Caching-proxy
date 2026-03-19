047b310767930ca05cf3358a23d1ba0190d24f20
Dargram <vpozodzuk@gmail.com>
2026-03-19 10:57:29 +0200

    Small fixes

--- proxy.py
+++ proxy.py
@@ -1,38 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import requests, argparse, json
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate",
-    "Connection": "keep-alive"
-}
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, required=True)
-    parser.add_argument("-o", "--origin", type=str, required=True)
+    parser.add_argument("-o", "--origin", type=str)
     args = parser.parse_args()
 
     with open("config.json", "r") as f:
         config = json.load(f)
 
-    target = config['proxy']['target']
+    origin = config['proxy']['target']
     app = Flask(__name__)
 
-    @app.route('/', defaults={"path":""})
-    @app.route("/<path:path>")
+    @app.route('/', defaults={"path":""}, methods=["GET"])
+    @app.route("/<path:path>", methods=["GET"])
     def proxy(path):
-        query = request.query_string.decode()
-        url = f"{args.origin}/{path}"
+        url = f"{args.origin or origin}/{path}"
+        headers = dict(request.headers)
+        headers.pop("Host", None)
 
-        if query:
-            url += f"?{query}"
+        resp = requests.request(url=url, headers=headers, params=request.args, method=request.method)
+        print("Status code:", resp.status_code)
+        print("\033[31m Final .url:", url, "\033[0m")
 
-        resp = requests.get(url=url, headers=HEADERS)
-        return resp.text
+        return Response(resp.content, resp.status_code)
 
-    app.run()
+    app.run(port=args.port)
+
 if __name__ == "__main__":
     main()
